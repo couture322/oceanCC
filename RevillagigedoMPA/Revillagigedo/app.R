9575801ab7991da36e1211627b571584cf443b7a
Jessica Couture <jcouture@ucsb.edu>
2022-03-15 15:53:31 -0700

    Creating gifs of ocean temps around revillagigedo MPA at different depths. run for epipelagic and mesopelagic. separate rmd for displaying gifs

---
+++ RevillagigedoMPA/Revillagigedo/app.R
@@ -0,0 +1,45 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Revillagigedo MPA Temperature projections with depth"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel( "Years to be displayed",
+            checkboxInput("2015","2050","2100")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
